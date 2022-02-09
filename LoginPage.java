7e3f84b51aed9e2a836874ce5a8852324844b3ad
Deepakraj15 <deepakraj15112002@gmail.com>
2022-02-09 18:37:38 +0530

    update

--- LoginPage.java
+++ LoginPage.java
@@ -7,16 +7,18 @@ public class LoginPage {
         JFrame logFrame = new JFrame("LOGIN - USER");
         JTextField textUser = new JTextField(); // usernme field
         JPasswordField textPass = new JPasswordField(); // password field
-        JButton logButton = new JButton("LOGIN");// login button
-        JLabel label1, label2;
-        GridBagConstraints gbc1, gbc2, gbc3, gbclabel;
+        JButton logButton = new JButton("LOGIN"); // login button
+        JLabel label1, label2, label3;
+        GridBagConstraints gbc1, gbc2, gbc3, gbclabel, gbchint;
         GridBagLayout layout = new GridBagLayout();
         logFrame.setLayout(layout);
 
         gbclabel = new GridBagConstraints();
 
         /** ----------------USERNAME------------------- */
+
         // username textfield creation and placement
+
         gbc1 = new GridBagConstraints();
         gbc1.fill = GridBagConstraints.HORIZONTAL;
         gbc1.ipadx = 120;
@@ -24,7 +26,9 @@ public class LoginPage {
         gbc1.gridx = 1;
         gbc1.gridy = 0;
         logFrame.add(textUser, gbc1); // adding username to the frame
+
         // label for username
+
         gbclabel.ipadx = 5;
         gbclabel.ipady = 10;
         gbclabel.gridx = 0;
@@ -33,7 +37,9 @@ public class LoginPage {
         logFrame.add(label1, gbclabel);
 
         /** ----------------PASSWORD------------------- */
+
         // password field creation and placemnet
+
         gbc2 = new GridBagConstraints();
         gbc2.fill = GridBagConstraints.HORIZONTAL;
         gbc2.ipadx = 120;
@@ -41,15 +47,20 @@ public class LoginPage {
         gbc2.gridx = 1;
         gbc2.gridy = 1;
         logFrame.add(textPass, gbc2); // adding password field to the frame
+
         // label for password
+
         gbclabel.ipadx = 5;
         gbclabel.ipady = 10;
         gbclabel.gridx = 0;
         gbclabel.gridy = 1;
         label2 = new JLabel("PASSWORD");
         logFrame.add(label2, gbclabel);
+
         /** ----------------BUTTON------------------- */
+
         // login button creation and placement
+
         gbc3 = new GridBagConstraints();
         gbc3.fill = GridBagConstraints.HORIZONTAL;
         gbc3.ipadx = 120;
@@ -58,6 +69,18 @@ public class LoginPage {
         gbc3.gridy = 2;
         logFrame.add(logButton, gbc3); // adding button to the frame
 
+        /** ------------hint message ------------------ */
+
+        gbchint = new GridBagConstraints();
+        gbchint.fill = GridBagConstraints.HORIZONTAL;
+        gbchint.ipadx = 120;
+        gbchint.ipady = 10;
+        gbchint.gridx = 1;
+        gbchint.gridy = 3;
+        label3 = new JLabel("HINT: Try 'password'");
+        label3.setVisible(false);
+        logFrame.add(label3, gbchint);
+
         logButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent ae) {
@@ -65,10 +88,12 @@ public class LoginPage {
                 MainPage mp = new MainPage();
                 String pass = new String(textPass.getPassword());
                 if (!pass.equals("password")) {
+                    label3.setVisible(true);
                     res.PassErrorText();
+
                 } else {
                     logFrame.dispose();
-                    mp.displayMain();
+                    mp.userMain();
                 }
             }
         });
@@ -78,7 +103,7 @@ public class LoginPage {
         logFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main function
         LoginPage ob;
         ob = new LoginPage();
         ob.userLogin();
--- MainPage.java
+++ MainPage.java
@@ -1,62 +1,41 @@
+import javax.lang.model.util.ElementScanner14;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class MainPage implements ActionListener {
-    // Creating Buttons
-    JButton btn1, btn2, btn3, btn4;
+public class MainPage implements ActionListener{
+    void userMain() {
+        JFrame frame = new JFrame(); // outer frame
+        JButton addUser, modUser, getCred, accUser; // buttons to operforms seperate functions
 
-    {
-        btn1 = new JButton("Check Balance");
-        btn2 = new JButton("Deposit");
-        btn3 = new JButton("Withdraw");
-        btn4 = new JButton("File a Complient");
+        // creating buttons
+
+        addUser = new JButton("Add New User");
+        modUser = new JButton("Modify User");
+        getCred = new JButton("Get Details");
+        accUser = new JButton("Access User");
     }
+    
+        public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        if(obj == addUser)
+        {
 
-    void displayMain() {
-        JFrame mainFrame = new JFrame("WELCOME");
-        JLabel action = new JLabel("Choose An Action ");
-        JPanel container = new JPanel();
-        container.setBackground(Color.CYAN);
-        mainFrame.add(action);
-
-        btn1.addActionListener(this);
-
-        btn2.addActionListener(this);
-
-        btn3.addActionListener(this);
-        // adding button to the container frame
-        container.add(btn1);
-        container.add(btn2);
-        container.add(btn3);
-        container.add(btn4);
-        btn4.addActionListener(this);
-        mainFrame.add(container);
-
-        container.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 30));
-
-        // setting frame properties
-        mainFrame.setVisible(true);
-        mainFrame.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 30));
-        mainFrame.setBounds(300, 100, 500, 500);
-        // actionlistener setup
-    }
-
-    public void actionPerformed(ActionEvent event) {
-        ResponsePage res = new ResponsePage();
-        Object ob = event.getSource();
-        if (ob == btn1) {
-
-            res.balanceCheck();
-        } else if (ob == btn2) {
-
-            res.cashDeposit();
-        } else if (ob == btn3) {
-
-            res.withdrawal();
-        } else {
-            res.feeback();
         }
+        else if(obj == modUser)
+        {
+
+        }
+        else if(obj == getCred)
+        {
+
+        }
+        else
+        {
+            
+        }
+        }
+
     }
 
 }
